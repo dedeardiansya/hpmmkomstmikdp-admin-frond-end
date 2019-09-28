b8f0acb468556c876014cc22bf7fb7b234adef75
dede ariansya <dedeariansya1@gmail.com>
2019-09-29 07:41:53 +0800

    create Login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,10 +1,42 @@
 <template>
   <div>
-    'dede'
+    <b-form @submit="onSubmit">
+      <b-form-input
+        v-model="form.email"
+        type="email"
+        placeholder="Alamat Email"
+        class="mb-4 mt-2"
+      />
+      <b-form-input
+        v-model="form.password"
+        type="password"
+        placeholder="Password"
+        class="mb-4 mt-2"
+      />
+
+      <b-button type="submit" variant="primary">Login</b-button>
+      <router-link to="/register" class="btn btn-secondary"
+        >Register</router-link
+      >
+    </b-form>
   </div>
 </template>
 <script>
 export default {
-  layout: 'auth'
+  layout: 'auth',
+  data() {
+    return {
+      form: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault()
+      alert(JSON.stringify(this.form))
+    }
+  }
 }
 </script>
