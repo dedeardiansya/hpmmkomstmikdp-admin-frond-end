578d73bf6b75ec0e5561ba1132aeff478d50e48d
dede ariansya <dedeariansya1@gmail.com>
2019-09-29 07:45:00 +0800

    create Register page

---
+++ pages/register.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <b-form @submit="onSubmit">
+      <b-form-input
+        v-model="form.name"
+        type="text"
+        placeholder="Nama"
+        class="mb-4 mt-2"
+      />
+      <b-form-input
+        v-model="form.email"
+        type="email"
+        placeholder="Alamat Email"
+        class="mb-4 mt-2"
+      />
+      <b-form-input
+        v-model="form.password"
+        type="password"
+        placeholder="Password"
+        class="mb-4 mt-2"
+      />
+
+      <b-button type="submit" variant="primary">Register</b-button>
+      <router-link to="/login" class="btn btn-secondary">Login</router-link>
+    </b-form>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'auth',
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+        name: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault()
+      alert(JSON.stringify(this.form))
+    }
+  }
+}
+</script>
