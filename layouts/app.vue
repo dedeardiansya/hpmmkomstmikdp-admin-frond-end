0c12bcdd13faa93eef48dc1d70b8c51d63ff7389
dede ariansya <dedeariansya1@gmail.com>
2019-09-29 20:13:14 +0800

    setup app layout

---
+++ layouts/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-layout">
+    <app-navbar />
+    <div class="p-2" />
+    <nuxt />
+  </div>
+</template>
+<script>
+import AppNavbar from '~/components/layouts/AppNavbar'
+export default {
+  components: {
+    AppNavbar
+  }
+}
+</script>
