54de0473e28fbde84cfde403228ce4678ba18ecc
dede ariansya <dedeariansya1@gmail.com>
2019-09-29 23:41:44 +0800

    Create article

---
+++ components/article/CreateArticle.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <b-button v-b-modal.modal-1 variant="secondary" block size="lg"
+      >CREATE ARTICLE</b-button
+    >
+    <b-modal id="modal-1" hide-footer title="Create Article">
+      <b-form @submit="onSubmit">
+        <b-form-input
+          v-model="form.title"
+          type="text"
+          class="mb-4 mt-2"
+          placeholder="Title"
+        />
+        <b-button type="submit" variant="primary">{{
+          loading ? 'Loading' : 'Create'
+        }}</b-button>
+      </b-form>
+    </b-modal>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        title: ''
+      },
+      loading: false
+    }
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault()
+      this.createArticle(this.form.title)
+    },
+    async createArticle(title) {
+      const token = this.$cookies.get('auth_token')
+      if (!token) return null
+      this.loading = true
+
+      try {
+        const { article, message } = await this.$axios.$post(
+          '/article/create',
+          { title },
+          {
+            headers: { authorization: 'bearer ' + token }
+          }
+        )
+        this.$router.push(`/article/${article.slug}/edit`)
+        this.$toast.success(message)
+      } catch (e) {
+        let mess
+        if (e.response) {
+          mess = e.response.data.message
+        } else {
+          mess = e.message
+        }
+        this.$toast.danger(mess)
+      }
+      this.loading = false
+    }
+  }
+}
+</script>
---
+++ pages/article/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container bg-white shadow-sm">
+    <div class="py-3">
+      <create-article />
+    </div>
+  </div>
+</template>
+<script>
+import CreateArticle from '~/components/article/CreateArticle'
+export default {
+  layout: 'app',
+  middleware: 'isLogin',
+  components: {
+    CreateArticle
+  }
+}
+</script>
