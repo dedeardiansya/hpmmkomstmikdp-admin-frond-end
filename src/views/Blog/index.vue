7a259e45fb75d8291401d957039d4c9cda2a166a
dedeard <dedeariansya1@gmail.com>
2019-11-04 00:27:54 +0800

    update blog page design

--- src/views/Blog/index.vue
+++ src/views/Blog/index.vue
@@ -4,16 +4,22 @@
       <div class="kt-portlet kt-portlet--height-fluid">
         <div class="kt-portlet__head">
           <div class="kt-portlet__head-toolbar">
-            <ul class="nav nav-pills nav-pills-sm nav-pills-label nav-pills-bold">
-              <li class="nav-item"><a class="nav-link active" href="#">Semua</a></li>
+            <ul
+              class="nav nav-pills nav-pills-sm nav-pills-label nav-pills-bold"
+            >
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Semua</a>
+              </li>
               <li class="nav-item"><a class="nav-link" href="#">Design</a></li>
-              <li class="nav-item"><a class="nav-link" href="#">Pemrograman</a></li>
-              <li class="nav-item"><a class="nav-link" href="#">Jaringan</a></li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pemrograman</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Jaringan</a>
+              </li>
             </ul>
           </div>
-          <div class="kt-portlet__head-toolbar">
-            <button class="btn btn-primary">Buat blog baru</button>
-          </div>
+          <create-blog-modal class="kt-portlet__head-toolbar" />
         </div>
 
         <div class="kt-portlet__body pb-0">
@@ -21,8 +27,11 @@
             <div class="kt-portlet__body p-0">
               <div class="kt-portlet__content">
                 <div class="kt-input-icon kt-input-icon--right">
-                  <input type="text" class="form-control form-control-lg bg-transparent border-0"
-                    placeholder="Search...">
+                  <input
+                    type="text"
+                    class="form-control form-control-lg bg-transparent border-0"
+                    placeholder="Search..."
+                  />
                   <span class="kt-input-icon__icon kt-input-icon__icon--right">
                     <span><i class="simple-icon-magnifier"></i></span>
                   </span>
@@ -35,24 +44,34 @@
         <div class="kt-portlet__body">
           <div class="kt-portlet" v-for="i in 5" :key="i">
             <div class="kt-portlet__body kt-ribbon">
-              <div class="kt-ribbon__target p-2 shadow-sm bg-white text-primary rounded font-weight-bold"
-                style="top: 10px; left: 10px;">
+              <div
+                class="kt-ribbon__target p-2 shadow-sm bg-white text-primary rounded font-weight-bold"
+                style="top: 10px; left: 10px;"
+              >
                 PUBLIC
               </div>
               <div class="kt-portlet__content">
                 <div class="row">
                   <div class="col-md-3 mb-3">
-                    <img src="@/assets/img/placeholder.svg" alt="">
+                    <img src="@/assets/img/placeholder.svg" alt="" />
                   </div>
                   <div class="col-md-9 mb-3">
                     <h3>Ini artickel pertama</h3>
-                    <p class="font-weight-bold"><span class="text-primary">Jaringan</span> - by <span
-                        class="text-primary">Admin</span>, 19 September 2019</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis corrupti non iure vitae aliquam,
-                      ipsam neque facere consequatur et repellendus deserunt ipsum eaque quis quas ad nihil aliquid
-                      nostrum eos!</p>
-                    <button class="btn btn-sm btn-label-primary">MAKE PUBLIC</button>&nbsp;
-                    <button class="btn btn-sm btn-label-info">EDIT</button>&nbsp;
+                    <p class="font-weight-bold">
+                      <span class="text-primary">Jaringan</span> - by
+                      <span class="text-primary">Admin</span>, 19 September 2019
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Debitis corrupti non iure vitae aliquam, ipsam neque
+                      facere consequatur et repellendus deserunt ipsum eaque
+                      quis quas ad nihil aliquid nostrum eos!
+                    </p>
+                    <button class="btn btn-sm btn-label-primary">
+                      MAKE PUBLIC</button
+                    >&nbsp;
+                    <button class="btn btn-sm btn-label-info">EDIT</button
+                    >&nbsp;
                     <button class="btn btn-sm btn-label-danger">DELETE</button>
                   </div>
                 </div>
@@ -64,3 +83,11 @@
     </div>
   </div>
 </template>
+<script>
+import createBlogModal from './components/CreateBlogModal'
+export default {
+  components: {
+    createBlogModal
+  }
+}
+</script>
