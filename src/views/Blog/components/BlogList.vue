dfa6b8091c74d65ef6426c585826f587788c04cb
dedeard <dedeariansya1@gmail.com>
2019-11-04 02:09:10 +0800

    lint fix

--- src/views/Blog/components/BlogList.vue
+++ src/views/Blog/components/BlogList.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="kt-portlet">
     <div class="kt-portlet__body kt-ribbon">
-      <div class="kt-ribbon__target p-2 shadow-sm bg-white text-primary rounded font-weight-bold"
-        style="top: 10px; left: 10px;">
+      <div
+        class="kt-ribbon__target p-2 shadow-sm bg-white text-primary rounded font-weight-bold"
+        style="top: 10px; left: 10px;"
+      >
         PUBLIC
       </div>
       <div class="kt-portlet__content">
@@ -17,13 +19,14 @@
               <span class="text-primary">Admin</span>, 19 September 2019
             </p>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Debitis corrupti non iure vitae aliquam, ipsam neque
-              facere consequatur et repellendus deserunt ipsum eaque
-              quis quas ad nihil aliquid nostrum eos!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+              corrupti non iure vitae aliquam, ipsam neque facere consequatur et
+              repellendus deserunt ipsum eaque quis quas ad nihil aliquid
+              nostrum eos!
             </p>
             <button class="btn btn-sm btn-label-primary">
-              MAKE PUBLIC</button>&nbsp;
+              MAKE PUBLIC</button
+            >&nbsp;
             <button class="btn btn-sm btn-label-info">EDIT</button>&nbsp;
             <button class="btn btn-sm btn-label-danger">DELETE</button>
           </div>
