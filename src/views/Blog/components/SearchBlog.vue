917fe0a94b68372fda5299f6ef9670d4baf0b2dd
dedeard <dedeariansya1@gmail.com>
2019-11-04 18:53:28 +0800

    search blog component

---
+++ src/views/Blog/components/SearchBlog.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="kt-portlet__body pb-0">
+    <div class="kt-portlet m-0 bg-light">
+      <div class="kt-portlet__body p-0">
+        <div class="kt-portlet__content">
+          <div class="kt-input-icon kt-input-icon--right">
+            <input
+              type="text"
+              class="form-control form-control-lg bg-transparent border-0"
+              placeholder="Search..."
+            />
+            <span class="kt-input-icon__icon kt-input-icon__icon--right">
+              <span><i class="simple-icon-magnifier"></i></span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/Blog/index.vue
+++ src/views/Blog/index.vue
@@ -21,38 +21,21 @@
           </div>
           <create-blog-modal class="kt-portlet__head-toolbar" />
         </div>
-
-        <div class="kt-portlet__body pb-0">
-          <div class="kt-portlet m-0 bg-light">
-            <div class="kt-portlet__body p-0">
-              <div class="kt-portlet__content">
-                <div class="kt-input-icon kt-input-icon--right">
-                  <input
-                    type="text"
-                    class="form-control form-control-lg bg-transparent border-0"
-                    placeholder="Search..."
-                  />
-                  <span class="kt-input-icon__icon kt-input-icon__icon--right">
-                    <span><i class="simple-icon-magnifier"></i></span>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
+        <search-blog></search-blog>
         <blog-lists></blog-lists>
       </div>
     </div>
   </div>
 </template>
 <script>
-import createBlogModal from './components/CreateBlogModal'
-import blogLists from './components/BlogLists'
+import CreateBlogModal from './components/CreateBlogModal'
+import BlogLists from './components/BlogLists'
+import SearchBlog from './components/SearchBlog'
 export default {
   components: {
-    createBlogModal,
-    blogLists
+    CreateBlogModal,
+    BlogLists,
+    SearchBlog
   }
 }
 </script>
