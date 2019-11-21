f650e7ecb96d6e89d2cfcf6bfa6b73b745e724f0
dedeard <dedeariansya1@gmail.com>
2019-11-21 20:00:22 +0800

    search blog

--- components/collections/blog/AppSearchBlog.vue
+++ components/collections/blog/AppSearchBlog.vue
@@ -5,6 +5,7 @@
         <div class="kt-portlet__content">
           <div class="kt-input-icon kt-input-icon--right">
             <input
+              v-model="search"
               type="text"
               class="form-control form-control-lg bg-transparent border-0"
               placeholder="Search..."
@@ -13,8 +14,63 @@
               <span><i class="simple-icon-magnifier"></i></span>
             </span>
           </div>
+          <div v-if="results.length && open" class="search-results bg-white">
+            <div class="search-result-title py-2 px-3">
+              <span class="heading-color">Result for :</span> {{ search }}
+            </div>
+            <nuxt-link
+              v-for="(result, i) in results"
+              :key="i"
+              :to="'blog/' + result.id"
+              class="search-result"
+            >
+              <h4 class="title heading-color">
+                {{ result.title }} ( {{ result.category }} )
+              </h4>
+              <h6 class="heading heading-color">{{ result.heading }}</h6>
+              <p class="description">{{ result.description }}</p>
+            </nuxt-link>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      search: '',
+      open: false,
+      results: []
+    }
+  },
+  watch: {
+    search() {
+      this.handleSearch()
+    }
+  },
+  methods: {
+    handleClickOutside() {
+      this.open = false
+    },
+    async handleSearch() {
+      if (!this.search) {
+        this.results = []
+        return
+      }
+      try {
+        const { results } = await this.$axios.$get(
+          '/admin/blog/search?value=' + this.search
+        )
+        this.results = results
+        this.open = true
+      } catch (e) {}
+      if (!this.search) {
+        this.results = []
+      }
+    }
+  }
+}
+</script>
