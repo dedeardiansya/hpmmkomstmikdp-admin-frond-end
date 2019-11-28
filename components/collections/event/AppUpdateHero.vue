36a9bce20fd8be552166452d1e390f8c71cb6e36
dedeard <dedeariansya1@gmail.com>
2019-11-28 15:02:06 +0800

    edit event

---
+++ components/collections/event/AppUpdateHero.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="hero-wrapper" :style="'background-image: url(' + heroImg + ')'">
+      <div class="form-hero card">
+        <input
+          ref="hero"
+          type="file"
+          name="hero"
+          accept="image/png,image/jpeg"
+          @change="onChange($event.target.files[0])"
+        />
+        <div class="message">
+          {{ uploadMessage }}
+        </div>
+        <i class="progress" :style="'width:' + uploadPercentage + '%'" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import FormData from 'form-data'
+import Placeholder from '~/assets/img/placeholder.svg'
+export default {
+  props: {
+    id: {
+      type: String,
+      default: '',
+      required: true
+    },
+    hero: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      form: null,
+      uploadPercentage: 0,
+      Placeholder
+    }
+  },
+  computed: {
+    uploadMessage() {
+      if (this.uploadPercentage === 0) return 'Drag or click to change'
+      if (this.uploadPercentage < 100)
+        return 'Uploading ' + this.uploadPercentage + '%'
+      else return 'Processing...'
+    },
+    config() {
+      return this.$store.getters.config
+    },
+    heroImg() {
+      return this.hero.length ? this.hero : Placeholder
+    }
+  },
+  methods: {
+    onUploadProgress(x) {},
+    async onChange(image) {
+      this.uploadPercentage = 0
+      this.form = new FormData()
+      this.form.append('image', image)
+      try {
+        const event = await this.$store.dispatch('event/UPDATE_EVENT_HERO', {
+          id: this.id,
+          form: this.form,
+          onUploadProgress: x => {
+            this.uploadPercentage = parseInt(
+              Math.round((x.loaded * 100) / x.total)
+            )
+          }
+        })
+        this.$emit('hero-updated', event.hero)
+      } catch (e) {
+        let mess
+        if (e.response) mess = e.response.data.message
+        else mess = e.message
+        this.$swal({ type: 'error', text: mess })
+      }
+      this.uploadPercentage = 0
+    }
+  }
+}
+</script>
