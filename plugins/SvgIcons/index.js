d2054bc5350ea4f483371e05e700f6000f12e84a
dedeard <dedeariansya1@gmail.com>
2019-12-07 01:52:38 +0800

    register user icon

--- plugins/SvgIcons/index.js
+++ plugins/SvgIcons/index.js
@@ -5,6 +5,7 @@ import SvgIconAngleDoubleLeft from './angleDoubleLeft'
 import SvgIconAngleDoubleRight from './angleDoubleRight'
 import SvgIconCipboardList from './cipboardList'
 import SvgIconBookOpen from './bookOpen'
+import SvgIconUser from './user'
 
 Vue.component('SvgIconGoogle', SvgIconGoogle)
 Vue.component('SvgIconEqualizer', SvgIconEqualizer)
@@ -12,3 +13,4 @@ Vue.component('SvgIconAngleDoubleLeft', SvgIconAngleDoubleLeft)
 Vue.component('SvgIconAngleDoubleRight', SvgIconAngleDoubleRight)
 Vue.component('SvgIconCipboardList', SvgIconCipboardList)
 Vue.component('SvgIconBookOpen', SvgIconBookOpen)
+Vue.component('SvgIconUser', SvgIconUser)
