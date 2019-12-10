ebed15f6eb7d17bae819535130da29ddc1c74e18
dedeard <dedeariansya1@gmail.com>
2019-12-11 00:53:13 +0800

    search anggota

--- components/collections/anggota/AppAnggotaLists.vue
+++ components/collections/anggota/AppAnggotaLists.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="kt-portlet__body">
     <div class="container-fluid">
-      <paginate tag="div" class="row" name="anggota" :list="anggota" :per="12">
+      <div class="kt-input-icon kt-input-icon--right bg-light mb-4">
+        <input
+          type="text"
+          placeholder="Search..."
+          v-model="search"
+          class="form-control form-control-lg bg-transparent border-0"
+        />
+        <span class="kt-input-icon__icon kt-input-icon__icon--right"
+          ><span><i class="simple-icon-magnifier"></i></span
+        ></span>
+      </div>
+      <paginate tag="div" class="row" name="anggota" :list="results" :per="12">
         <app-anggota-list
           v-for="data in paginated('anggota')"
           :key="data.id"
@@ -24,6 +35,7 @@
   </div>
 </template>
 <script>
+import Fuse from 'fuse.js'
 import AppAnggotaList from './AppAnggotaList'
 export default {
   components: {
@@ -31,12 +43,48 @@ export default {
   },
   data() {
     return {
-      paginate: ['anggota']
+      paginate: ['anggota'],
+      search: '',
+      results: []
+    }
+  },
+  watch: {
+    search() {
+      this.handleSearch()
     }
   },
   computed: {
     anggota() {
       return this.$store.getters['anggota/anggota'].sortBy(o => o.cadreYear)
+    }
+  },
+  created() {
+    this.results = this.$store.getters['anggota/anggota'].sortBy(
+      o => o.cadreYear
+    )
+  },
+  methods: {
+    searchAnggota(val) {
+      const fuse = new Fuse(this.anggota, {
+        shouldSort: true,
+        threshold: 0.5,
+        location: 0,
+        distance: 100,
+        maxPatternLength: 32,
+        minMatchCharLength: 1,
+        keys: ['name', 'placeOfBirth', 'cadreYear']
+      })
+      return fuse.search(val)
+    },
+    handleSearch() {
+      if (!this.search) {
+        this.results = this.anggota
+        return
+      }
+      this.results = this.searchAnggota(this.search)
+      if (!this.search) {
+        this.results = this.anggota
+      }
     }
   }
 }
